ad40c8db4c4ee56049afd11e64100c75a57b08dc
alwayslearningwebdev <57058273+alwayslearningwebdev@users.noreply.github.com>
2020-05-27 22:38:54 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const nav__burger = document.getElementById('nav__burger');
+const nav__list = document.querySelector('nav__list');
+
+nav__burger.addEventListener('click', () => {
+nav__list.classList.toggle('show');
+
+});
+
+
